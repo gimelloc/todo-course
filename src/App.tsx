4d168d7e85cb95aea20ff65668b5947573122adb
gimelloc <gislainemcasa@gmail.com>
2025-06-01 19:17:56 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import Text from "./components/text";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import Icon from "./components/icon";
+import Badge from "./components/badge";
+import Button from "./components/button";
+
+export default function App() {
+  return (
+    <div className="grid gap-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Olá mundo!
+        </Text>
+
+        <Text className="text-green-base">Olá mundo!</Text>
+
+        <Text variant="body-md-bold" className="text-green-base">
+          Olá mundo!
+        </Text>
+      </div>
+
+      <div className="flex gap-1">
+        <Icon svg={TrashIcon} className="fill-pink-base" />
+      </div>
+
+      <div>
+        <Badge variant="secondary">5</Badge>
+        <Badge variant="primary">2 de 5</Badge>
+      </div>
+
+      <div>
+        <Button icon={TrashIcon}>Nova tarefa</Button>
+      </div>
+    </div>
+  );
+}
